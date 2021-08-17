75cc31883517c44f5f742ca9c792903935450707
Anton <enitefall81@gmail.com>
2021-08-17 14:00:55 -0700

    Fix the initial search delay

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,68 +2,73 @@ import React, {useState, useEffect} from "react"
 import axios from 'axios'
 
 let Search = () => {
-let [searchTerm, setSearchTerm] = useState('haskell')
-let [results, setResults] = useState([])
+  let [searchTerm, setSearchTerm] = useState('haskell')
+  let [results, setResults] = useState([])
 
   console.log(results)
 
   let onSearch = (input) => {
-  setSearchTerm(input)
+    setSearchTerm(input)
   }
-useEffect(() => {
-  let search = async () => {
+  useEffect(() => {
+    let search = async () => {
 
-    let {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-      params: {
-        action: 'query',
-        list: 'search',
-        format: 'json',
-        origin: '*',
-        srsearch: searchTerm
-      }
-    })
-  setResults(data.query.search)
-  }
-
-  let timeoutId = setTimeout(() => {
-    if (searchTerm) {
-      search()
+      let {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          format: 'json',
+          origin: '*',
+          srsearch: searchTerm
+        }
+      })
+      setResults(data.query.search)
     }
-  }, 500)
-  return () => {
-    clearTimeout(timeoutId)
-  }
-}, [searchTerm])
+
+    if (searchTerm && !results.length) {
+      search()
+    } else
+      {
+        let timeoutId = setTimeout(() => {
+          if (searchTerm) {
+            search()
+          }
+        }, 500)
+        return () => {
+          clearTimeout(timeoutId)
+        }
+      }
+    }, [searchTerm])
 
   let renderedList = results.map((result, i) => {
     return <div className="item" i={result.pageid}>
       <div className="right floated content">
-      <a className="ui button"
-      href={`https://en.wikipedia.org?curid=${result.pageid}`}
-      >
-        Go
-      </a>
+        <a className="ui button"
+           href={`https://en.wikipedia.org?curid=${result.pageid}`}
+        >
+          Go
+        </a>
       </div>
       <div className="content">
         <div className="header">
-        {result.title}
+          {result.title}
         </div>
-        <span dangerouslySetInnerHTML={{ __html:result.snippet}}></span>
-        </div>
+        <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+      </div>
     </div>
   })
 
   return <div>
- <div className="ui form">
-   <div className="field">
-     <label>Enter Search Term</label>
-     <input value={searchTerm} onInput={(e) => onSearch(e.target.value)} className="input"/>
+    <div className="ui form">
+      <div className="field">
+        <label>Enter Search Term</label>
+        <input value={searchTerm} onInput={(e) => onSearch(e.target.value)} className="input"/>
       </div>
- </div>
+    </div>
     <div className="ui celled list">
       {renderedList}
     </div>
- </div>
+  </div>
 }
 
 export default Search
