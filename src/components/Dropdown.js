5e0e9e8cca61055c8c1c7365060d3f3fab2c960b
Anton Durandin <enitefall81@gmail.com>
2021-08-22 15:43:54 -0700

    Showing selected co;or with OnClickHanlder

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,24 +1,28 @@
 import React from "react"
 
-let Dropdown = ({options}) => {
- let renderedListOfOptions =  options.map((option) => {
-     return <div key={option.value} className="item">
-            {option.label}
-            </div>
+let Dropdown = ({options, onSelectedChange, selected}) => {
+  let renderedListOfOptions = options.map((option) => {
+    return <div
+        key={option.value}
+        className="item"
+        onClick={() => onSelectedChange(option)}
+    >
+      {option.label}
+    </div>
 
- })
+  })
   return <div className="ui form">
     <div className="field">
       <label className="label">Select a label</label>
       <div className="ui selection dropdown visible active">
-      <i className="dropdown icon"></i>
-        <div className="text">Select Color</div>
+        <i className="dropdown icon"></i>
+        <div  className="text">{selected.label}</div>
         <div className="menu visible transition">
           {renderedListOfOptions}
         </div>
       </div>
     </div>
-    </div>
+  </div>
 
 
 }
