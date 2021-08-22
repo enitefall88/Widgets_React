04f48610a60f24a96d8df7a0bef5d7842c31b6c9
Anton Durandin <enitefall81@gmail.com>
2021-08-22 15:52:31 -0700

    Remove selected color from the options list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,15 +2,18 @@ import React from "react"
 
 let Dropdown = ({options, onSelectedChange, selected}) => {
   let renderedListOfOptions = options.map((option) => {
-    return <div
-        key={option.value}
-        className="item"
-        onClick={() => onSelectedChange(option)}
-    >
-      {option.label}
-    </div>
-
+    if(option.value === selected.value) {
+      return null
+    }
+      return <div
+          key={option.value}
+          className="item"
+          onClick={() => onSelectedChange(option)}
+      >
+        {option.label}
+      </div>
   })
+
   return <div className="ui form">
     <div className="field">
       <label className="label">Select a label</label>
