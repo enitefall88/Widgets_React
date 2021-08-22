217aca290df0fdbebc8a848434521178891d3cac
Anton Durandin <enitefall81@gmail.com>
2021-08-22 15:26:16 -0700

    Implement Dropdown list with colors

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,9 +7,20 @@ let Dropdown = ({options}) => {
             </div>
 
  })
-  return <h1>
-    {renderedListOfOptions}
-  </h1>
+  return <div className="ui form">
+    <div className="field">
+      <label className="label">Select a label</label>
+      <div className="ui selection dropdown visible active">
+      <i className="dropdown icon"></i>
+        <div className="text">Select Color</div>
+        <div className="menu visible transition">
+          {renderedListOfOptions}
+        </div>
+      </div>
+    </div>
+    </div>
+
+
 }
 
 export default Dropdown
