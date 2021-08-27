76ccfebf03f6650f58a181e08982b573c38366ac
Anton Durandin <enitefall81@gmail.com>
2021-08-26 22:02:46 -0700

    UseRef fixed

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from "react"
+import React, {useEffect, useState, useRef } from "react"
 
 let Dropdown = ({options, onSelectedChange, selected}) => {
   let [open, setOpen] = useState(false)
