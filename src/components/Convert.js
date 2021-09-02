a7f10a4a0e8a5319872364f4774208f23f0aeab9
Anton Durandin <enitefall81@gmail.com>
2021-09-02 14:05:15 -0700

    Add doTranslation function

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,18 +1,24 @@
-import React, { useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import axios from 'axios'
 
 export default function Convert({language, text}) {
+  let [translated, setTranlsated] = useState('')
+
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-          params: {
-            q: text,
-            target: language.value,
-            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-          }
-        })
+    let doTranslation = async() => {
+      let {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      })
+      setTranlsated(data.data.translations[0].translatedText)
+      doTranslation()
+    }
   }, [language, text])
 
   return <div>
-
+    {translated}
   </div>
 }
