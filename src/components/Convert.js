e87fa5d51b57b0003c395727b058c1f5d1bb8f4f
Anton Durandin <enitefall81@gmail.com>
2021-09-07 11:35:14 -0700

    Comments on {data} and destructuring

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -7,19 +7,22 @@ export default function Convert({language, text}) {
   useEffect(() => {
     let doTranslation = async() => {
       let {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+       //{data} is a destructured property from response object
+        // at the same time destructure the data and assign it as the variable data
         params: {
           q: text,
           target: language.value,
           key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
         }
+
       })
+
       setTranslated(data.data.translations[0].translatedText)
     }
     doTranslation()
   }, [language, text])
 
   return <div>
-    {translated}
-
+    <h1>{translated}</h1>
   </div>
 }
