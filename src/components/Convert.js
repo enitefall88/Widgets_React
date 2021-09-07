535fbd3666c87a1572575bfdcbefe0a9d7782fe3
Anton Durandin <enitefall81@gmail.com>
2021-09-07 13:22:31 -0700

    Limit request with a 1sec timer with debounce

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,6 +3,15 @@ import axios from 'axios'
 
 export default function Convert({language, text}) {
   let [translated, setTranslated] = useState('')
+  let [debouncedText, setDebouncedText] = useState('')
+
+  useEffect(()=> {
+    let timerId = setTimeout(() => setDebouncedText(text), 1000)
+    return ()=> {
+      clearTimeout(timerId)
+      console.log(debouncedText)
+    }
+  }, [text])
 
   useEffect(() => {
     let doTranslation = async() => {
@@ -10,7 +19,7 @@ export default function Convert({language, text}) {
        //{data} is a destructured property from response object
         // at the same time destructure the data and assign it as the variable data
         params: {
-          q: text,
+          q: debouncedText,
           target: language.value,
           key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
         }
@@ -20,7 +29,7 @@ export default function Convert({language, text}) {
       setTranslated(data.data.translations[0].translatedText)
     }
     doTranslation()
-  }, [language, text])
+  }, [language, debouncedText])
 
   return <div>
     <h1>{translated}</h1>
