6e135774656ab93d88479ebe059487e28124500c
Anton Durandin <enitefall81@gmail.com>
2021-09-24 10:35:02 -0700

    Add paths for react routing

--- src/App.js
+++ src/App.js
@@ -9,34 +9,59 @@ let items = [
     title: 'What is React?',
     content: 'React is front-end framework'
   },
-  { title: 'Why use React?',
+  {
+    title: 'Why use React?',
     content: 'Makes life easier'
-   },
-  { title: 'How do you use React?',
+  },
+  {
+    title: 'How do you use React?',
     content: 'By creating components'
   }
 ]
 
 let options = [
-    {
-      label: 'The Color Red',
-      value: 'red'
-    },
-     {
-      label: 'The Color Blue',
-      value: 'blue'
-    },
-     {
-      label: 'The Color Green',
-      value: 'green'
-    }
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  }
 ]
 
+let showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items}/>
+  }
+}
+
+let showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search/>
+  }
+}
+
+let showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown/>
+  }
+}
+
+let showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate/>
+  }
+}
 
 
 function App() {
   return <div>
-<Translate/>
+    {showAccordion()}
   </div>
 
 }
